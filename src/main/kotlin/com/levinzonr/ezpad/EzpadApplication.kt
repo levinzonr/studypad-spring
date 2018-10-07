62f164a1e25df334980bb06108070cfec214cdf5
Roman Levinzon <levinzon.roman@gmail.com>
2018-10-07 14:11:39 +0200

    gradle fix

--- src/main/kotlin/com/levinzonr/ezpad/EzpadApplication.kt
+++ src/main/kotlin/com/levinzonr/ezpad/EzpadApplication.kt
@@ -1,11 +1,18 @@
 package com.levinzonr.ezpad
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EzpadApplication
+class EzpadApplication {
 
-fun main(args: Array<String>) {
-    runApplication<EzpadApplication>(*args)
+    //Using generated security password: d571634d-2999-4b80-a80d-022b0600e511
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(EzpadApplication::class.java, *args)
+        }
+    }
+
 }
