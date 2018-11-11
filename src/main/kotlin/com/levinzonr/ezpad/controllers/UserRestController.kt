dce09eb87808ae64387d5a9b2eb0a6d1f3899123
Roman Levinzon <levinzon.roman@gmail.com>
2018-11-11 15:27:36 +0100

    Return token after registration

--- src/main/kotlin/com/levinzonr/ezpad/controllers/UserRestController.kt
+++ src/main/kotlin/com/levinzonr/ezpad/controllers/UserRestController.kt
@@ -1,14 +1,18 @@
 package com.levinzonr.ezpad.controllers
 
+import com.levinzonr.ezpad.domain.model.TokenResponse
 import com.levinzonr.ezpad.domain.responses.UserResponse
 import com.levinzonr.ezpad.domain.payload.CreateUserPayload
 import com.levinzonr.ezpad.domain.payload.UpdateUserPayload
 import com.levinzonr.ezpad.security.EzpadUserDetails
 import com.levinzonr.ezpad.services.UserService
+import com.levinzonr.ezpad.utils.baseUrl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
+import javax.servlet.ServletRequest
+import javax.servlet.http.HttpServletRequest
 import javax.validation.Valid
 
 @RestController
@@ -25,9 +29,12 @@ class UserRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createNewUser(@Valid @RequestBody payload: CreateUserPayload): UserResponse {
+    fun createNewUser(@Valid @RequestBody payload: CreateUserPayload,
+                      req: HttpServletRequest): TokenResponse {
         val user = userService.createUser(payload.email!!, payload.password!!, payload.firstName, payload.lastName, null)
-        return user.toResponse()
+        return RestAuthHelper.authRedirect(req.baseUrl, user.email, payload.password).also {
+            it.user = user.toResponse()
+        }
     }
 
 
