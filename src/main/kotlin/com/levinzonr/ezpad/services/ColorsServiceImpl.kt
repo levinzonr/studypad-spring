6bfd76e8024d2a7c4fa0457cf36283ab45fe1220
Roman Levinzon <levinzon.roman@gmail.com>
2018-12-25 14:55:40 +0100

    Colors fix

--- src/main/kotlin/com/levinzonr/ezpad/services/ColorsServiceImpl.kt
+++ src/main/kotlin/com/levinzonr/ezpad/services/ColorsServiceImpl.kt
@@ -9,14 +9,14 @@ class ColorsServiceImpl : ColorsService {
 
     private val colorsList: List<String> = listOf(
             "#f5f7fa,#c3cfe2",
-            "#89f7fe , #66a6ff",
-            "#13547a , #80d0c7",
+            "#89f7fe,#66a6ff",
+            "#13547a,#80d0c7",
             "#67b26f,#4ca2cd",
             "#f3904f,#3b4371",
             "#ee0979,#ff6a00",
             "#ff00cc,#333399",
             "#56ab2f,#a8e063",
-            "000428,#004e92"
+            "#000428,#004e92"
 
     )
 
