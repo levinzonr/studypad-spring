a7aa825c2d7f59b2f1864aa840d23d0a9e797233
Roman Levinzon <levinzon.roman@gmail.com>
2019-03-01 09:18:48 +0500

    throw 401 with bad token

--- src/main/kotlin/com/levinzonr/ezpad/security/firebase/FirebaseAuthTokenFilter.kt
+++ src/main/kotlin/com/levinzonr/ezpad/security/firebase/FirebaseAuthTokenFilter.kt
@@ -1,6 +1,9 @@
 package com.levinzonr.ezpad.security.firebase
 
+import com.google.firebase.auth.FirebaseAuthException
+import com.levinzonr.ezpad.domain.errors.AuthException
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
 import java.lang.RuntimeException
 import javax.servlet.FilterChain
@@ -14,9 +17,17 @@ class FirebaseAuthTokenFilter : AbstractAuthenticationProcessingFilter("/api/**"
         val authToken = request?.getHeader(TOKEN_HEADER)
         if (authToken.isNullOrEmpty()) throw RuntimeException("Invalid token")
         print("attemprtAuth: $authToken")
-        return authenticationManager.authenticate(FirebaseAuthToken(authToken))
+        return try {
+            authenticationManager.authenticate(FirebaseAuthToken(authToken))
+        } catch (exception: Exception) {
+            throw AuthException("${exception.message}")
+        }
     }
 
+    override fun unsuccessfulAuthentication(request: HttpServletRequest?, response: HttpServletResponse?, failed: AuthenticationException?) {
+        super.unsuccessfulAuthentication(request, response, failed)
+        print("FAILDED")
+    }
 
     override fun successfulAuthentication(request: HttpServletRequest?, response: HttpServletResponse?, chain: FilterChain?, authResult: Authentication?) {
         super.successfulAuthentication(request, response, chain, authResult)
